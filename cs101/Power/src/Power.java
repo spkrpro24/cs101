2dbdefc515c82f287f82a6fb9b4c869aba670c6f
jeremyallison1 <jeremy.allison@clarecontrols.com>
2015-02-08 17:07:20 -0500

    Power using a recursive method
    Exercise 6.9. Write a recursive method called power that takes a double
    x
    and an integer n and that returns xn.

---
+++ cs101/Power/src/Power.java
@@ -0,0 +1,17 @@
+public class Power {
+	public static double power(double x, int n) { 		// Recursion
+		if (n == 0) 									// base case (equality)
+			return 1;
+		else if (n > 0)									 // general case
+			return x * power(x, n - 1);
+		else											// 2nd general case //int square = Math.pow(i, 2)
+			return (1.0 / power(x, Math.abs(n) - 1));
+	}
+
+	public static void main(String[] args) {
+		System.out.println(power(2, -9));
+		System.out.println(power(4, 6));
+		System.out.println(power(3, 0));
+
+	}
+}
